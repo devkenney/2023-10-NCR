3a9bb9a36c8aba019a9f9408741da4b49c78e22c
Devin Kenney <devin.kenney111@gmail.com>
2024-01-26 11:45:13 -0500

    week 13 day 2

--- unit_3/fruits/server.js
+++ unit_3/fruits/server.js
@@ -2,12 +2,55 @@ const express = require('express');
 const fruits = require('./models/fruits.js');
 const app = express();
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use((req, res, next) => {
+  console.log('I run for all routes');
+  next();
+})
+
+// INDUCES
+
+// Index
+
 app.get('/fruits', (req, res) => {
-  res.send(fruits);
+  res.render('Index', { fruits: fruits });
 });
 
+// New
+
+app.get('/fruits/new', (req, res) => {
+  res.render('New');
+})
+
+// Delete
+
+// Update
+
+// Create
+
+app.post('/fruits', (req, res) => {
+  if (req.body.readyToEat === 'on') {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  fruits.push(req.body)
+  console.log(fruits)
+  res.redirect('/fruits')
+})
+
+// Edit
+
+// Show
+
 app.get('/fruits/:index', (req, res) => {
-  res.send(fruits[req.params.index]);
+  res.render('Show', {
+    fruit: fruits[req.params.index]
+  });
 });
 
 app.listen(3000, () => {
